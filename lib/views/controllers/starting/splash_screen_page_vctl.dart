447de6747aa6aeb56c0b0a8963c5ba5e17ac0ba8
Yves AMANGOUA <mr.amangoua@gmail.com>
2025-11-10 19:06:45 +0000

    feat: Refactor splash screen controller to improve error handling and update import paths

--- lib/views/controllers/starting/splash_screen_page_vctl.dart
+++ lib/views/controllers/starting/splash_screen_page_vctl.dart
@@ -1,17 +1,23 @@
-import 'package:app_couture/data/models/user.dart';
-import 'package:app_couture/tools/components/session_manager_view_controller.dart';
-import 'package:app_couture/views/static/auth/auth_home_page.dart';
-import 'package:app_couture/views/static/home/home_windows.dart';
+import 'package:ateliya/data/models/user.dart';
+import 'package:ateliya/tools/components/session_manager_view_controller.dart';
+import 'package:ateliya/tools/models/data_response.dart';
+import 'package:ateliya/views/static/auth/auth_home_page.dart';
+import 'package:ateliya/views/static/home/home_windows.dart';
 import 'package:get/get.dart';
 
 class SplashScreenPageVctl extends SessionManagerViewController {
   Future<void> redirect() async {
-    await Future.delayed(const Duration(seconds: 2));
-    final cachedUser = await User.getUserFromCache();
-    if (cachedUser != null) {
-      user = cachedUser;
-      Get.off(() => const HomeWindows());
-    } else {
+    try {
+      await Future.delayed(const Duration(seconds: 2));
+      final cachedUser = await User.getUserFromCache();
+      if (cachedUser != null) {
+        user = cachedUser;
+        Get.off(() => const HomeWindows());
+      } else {
+        Get.off(() => const AuthHomePage());
+      }
+    } catch (e, t) {
+      DataResponse.error(systemError: e, systemtraceError: t);
       Get.off(() => const AuthHomePage());
     }
   }
