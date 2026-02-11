4dec965285c8944d8b357ae7a1ee495276e429f0
Yves AMANGOUA <mr.amangoua@gmail.com>
2026-02-11 19:11:11 +0000

    chore: update app version to 1.0.1 and build number to 8

--- lib/tools/constants/env.dart
+++ lib/tools/constants/env.dart
@@ -7,8 +7,8 @@ import 'package:google_fonts/google_fonts.dart';
 
 abstract class Env {
   static const String appName = 'Ateliya';
-  static const String appVersion = '1.0.0';
-  static const int buildVersion = 7;
+  static const String appVersion = '1.0.1';
+  static const int buildVersion = 8;
   static final fontFamily = GoogleFonts.poppins().fontFamily;
 
   static const defaultDevise = Currency("Fcfa", code: "XOF", decimalDigits: 0);
