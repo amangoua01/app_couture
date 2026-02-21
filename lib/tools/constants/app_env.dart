f6954e6b73a073064f2dfc4ab078f4840e357e1a
Yves AMANGOUA <mr.amangoua@gmail.com>
2026-02-21 05:48:12 +0000

    refactor: Move network configuration directly into the `AppEnv` enum and simplify `Env` class's `baseUrl` getter.

--- lib/tools/constants/app_env.dart
+++ lib/tools/constants/app_env.dart
@@ -1,1 +1,20 @@
-enum AppEnv { dev, prod }
+import 'package:ateliya/tools/models/netword_config.dart';
+
+enum AppEnv {
+  dev(
+    networdConfig: NetwordConfig(
+      host: "backend.ateliya.com",
+      scheme: "https",
+    ),
+  ),
+  prod(
+    networdConfig: NetwordConfig(
+      host: "backendprod.ateliya.com",
+      scheme: "https",
+    ),
+  );
+
+  final NetwordConfig networdConfig;
+
+  const AppEnv({required this.networdConfig});
+}
