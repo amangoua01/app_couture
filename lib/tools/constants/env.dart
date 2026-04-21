0aeea992f054a1749f475c7300d2407ddec31aa1
Yves AMANGOUA <mr.amangoua@gmail.com>
2026-04-21 18:08:03 +0000

    chore: bump app version to 1.0.17+24

--- lib/tools/constants/env.dart
+++ lib/tools/constants/env.dart
@@ -8,8 +8,8 @@ import 'package:google_fonts/google_fonts.dart';
 
 abstract class Env {
   static const String appName = 'Ateliya';
-  static const String appVersion = '1.0.16';
-  static const int buildVersion = 23;
+  static const String appVersion = '1.0.17';
+  static const int buildVersion = 24;
   static final fontFamily = GoogleFonts.poppins().fontFamily;
 
   static const String defaultPassword = "admin93K";
