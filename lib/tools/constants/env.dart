736913bd979a6f7b8b39159d7bf22140a457b6ab
Yves AMANGOUA <mr.amangoua@gmail.com>
2026-07-03 22:37:46 +0000

    chore: update app version to 1.1.0 and build number to 30 in env and pubspec files

--- lib/tools/constants/env.dart
+++ lib/tools/constants/env.dart
@@ -8,8 +8,8 @@ import 'package:google_fonts/google_fonts.dart';
 
 abstract class Env {
   static const String appName = 'Ateliya';
-  static const String appVersion = '1.0.21';
-  static const int buildVersion = 28;
+  static const String appVersion = '1.1.0';
+  static const int buildVersion = 30;
   static final fontFamily = GoogleFonts.poppins().fontFamily;
 
   static const String defaultPassword = "admin93K";
