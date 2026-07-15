26ef32ea6fcbefe99d122725ea648ad3a80fb455
Emericktahe <164502398+Emericktahe@users.noreply.github.com>
2026-07-15 18:19:09 +0000

    feat: add Mall Ya module and update registration page UI navigation

---
+++ lib/views/static/mall_ya/mall_ya_home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MallYaHomePage extends StatelessWidget {
+  const MallYaHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Mall Ya")),
+      body: const Center(child: Text("Mall Ya")),
+    );
+  }
+}
